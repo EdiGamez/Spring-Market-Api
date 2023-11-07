4fb3f85a5356fb2029c0b8dd0a436be8227015ff
Eduardo F. Gamez <EdiGamez11@outlook.com>
2023-11-07 12:57:26 -0600

    Update Swagger

--- src/main/java/com/projecto/market/MarketApplication.java
+++ src/main/java/com/projecto/market/MarketApplication.java
@@ -3,7 +3,9 @@ package com.projecto.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class MarketApplication {
 
 	public static void main(String[] args) {
